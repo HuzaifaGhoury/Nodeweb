cc879420c8bf4057cf963d9115931d1792a5ec7f
HuzaifaGhoury <huzaifaghourydev@gmail.com>
2024-01-11 04:37:36 -0800

    Create APIs for Teacher , Student and Admin

--- index.js
+++ index.js
@@ -30,16 +30,30 @@ app.get("/", (req, res) => {
   });
 });
 
+// Signup API:
 app.post("/signup", async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, email, password, role } = req.body;
 
   try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({
+          code: 400,
+          message: "Email already in use. Choose a different one.",
+        });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({
       username,
       email,
+      role,
       password: hashedPassword,
     });
+
     await newUser.save();
     res.status(200).json({ code: 200, message: "User saved successfully" });
   } catch (error) {
@@ -48,6 +62,7 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Login API:
 app.post("/signin", async (req, res) => {
   const { email, password } = req.body;
 
@@ -67,13 +82,19 @@ app.post("/signin", async (req, res) => {
     }
 
     const token = jwt.sign(
-      { userId: user._id, username: user.username, email: user.email },
+      {
+        userId: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
       secretKey,
       {
         expiresIn: "1h",
       }
     );
 
+    res.cookie("jwtToken", token, { httpOnly: true });
     res.status(200).json({ token });
   } catch (error) {
     console.error(error);
@@ -81,10 +102,9 @@ app.post("/signin", async (req, res) => {
   }
 });
 
+// Verify JwToken:
 function verifyToken(req, res, next) {
   const token = req.cookies.jwtToken;
-  // console.log(token);
-
   if (!token) {
     return res.status(401).json({ error: "Access denied" });
   }
@@ -92,18 +112,57 @@ function verifyToken(req, res, next) {
   try {
     const decoded = jwt.verify(token, secretKey);
     req.userId = decoded.userId;
+    req.userRole = decoded.role;
     next();
   } catch (error) {
     res.status(401).json({ error: "Invalid token" });
   }
 }
 
+// Authentication middleware:
 app.get("/user", verifyToken, (req, res) => {
   res.status(200).json({ message: "Protected route accessed" });
 });
+
+// Logout API:
 app.get("/logout", (req, res) => {
+  res.clearCookie("jwtToken");
   res.json({ message: "Logout successful" });
 });
+
+// Middleware to check user roles:
+function checkUserRole(role) {
+  return (req, res, next) => {
+    if (req.userRole === role) {
+      next();
+    } else {
+      res.status(403).json({ error: "Access denied" });
+    }
+  };
+}
+
+// Role based routes:
+app.get("/admindashboard", verifyToken, checkUserRole("admin"), (req, res) => {
+  res.json({ message: "Admin access granted" });
+});
+
+app.get(
+  "/teacherdashboard",
+  verifyToken,
+  checkUserRole("teacher"),
+  (req, res) => {
+    res.json({ message: "Teacher access granted" });
+  }
+);
+
+app.get(
+  "/studentdashboard",
+  verifyToken,
+  checkUserRole("student"),
+  (req, res) => {
+    res.json({ message: "Student access granted" });
+  }
+);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
